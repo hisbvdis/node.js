08b07f5fb58948432ca2cde24dc147294cd6821e
hisbvdis <hisbvdis@gmail.com>
2023-05-13 21:56:33 +0300

    Changes

--- express/2expanded/middleware/error.js
+++
@@ -1,4 +0,0 @@
-export default (req, res) => {
-  res.status(404);
-  res.send("404 | not found");
-}
--- express/2expanded/routes/login.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-router.post("/", (req, res) => {
-  res.status(201);
-  res.json({ id: 1, mail: "test@mail.ru" });
-})
-
-export default router;
